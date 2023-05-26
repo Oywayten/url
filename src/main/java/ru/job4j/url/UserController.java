969710020396f7a4f934c9b4c9131156a90ddef8
Oywayten <oywayten@gmail.com>
2023-05-26 13:19:44 +0300

    delete comments

--- src/main/java/ru/job4j/url/UserController.java
+++ src/main/java/ru/job4j/url/UserController.java
@@ -21,20 +21,12 @@ public class UserController {
         this.encoder = encoder;
     }
 
-    /*
-    curl -H "Content-Type: application/json" -X POST -d {"""username""":"""admin""","""password""":"""password"""} "localhost:8080/users/sign-up"
-
-    curl -i -H "Content-Type: application/json" -X POST -d {"""username""":"""admin""","""password""":"""password"""} "localhost:8080/login"
-     */
     @PostMapping("/sign-up")
     public void signUp(@RequestBody Person person) {
         person.setPassword(encoder.encode(person.getPassword()));
         users.save(person);
     }
 
-    /*
-    curl -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY4NTk1OTU5NX0.eEjz1cO90mvwDPFgfPrJ7UWjm4fvzTb7gWi9DwoStHDl5wIHFuJ-HQyuvskAletwWl2wU-HAmj_mpOULfAvm2w" http://localhost:8080/users/all
-     */
     @GetMapping("/all")
     public List<Person> findAll() {
         return users.findAll();
